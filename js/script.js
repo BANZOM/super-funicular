22aafc0a1901fee28e7b09a4a4a11da433cffc7e
BANZOM <aditya.panwar.2208@gmail.com>
2023-06-22 16:19:01 +0530

    login change

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     document.querySelector("body").classList.toggle("active");
 })
+
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !email || !password) {
+      alert("Please fill out all fields.");
+      return false;
+    }
+
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      return false;
+    }
+
+    return true;
+  }
