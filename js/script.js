a62aea1cd9c24aef5b7bd9d6bd71286e55e9f166
BANZOM <aditya.panwar.2208@gmail.com>
2023-06-26 02:58:56 +0530

    password regex

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ function validateForm() {
     }
 
     // Check if password contains at least one number and one letter
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     if (!passwordRegex.test(password)) {
         alert("Password must contain at least one number and one letter.");
         return false;
